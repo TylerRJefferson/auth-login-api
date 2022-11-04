83c74c035d30f605b0562cb9c48dd8f0e9680b83
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-04 15:52:51 -0400

    update user

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import { userLogin, addNewUser, updateUser } from './src/users.js';
+
+const PORT = 3030
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post('/login', userLogin)
+app.post('/users', addNewUser)
+app.patch('/users/:uid', updateUser)
+
+app.listen(PORT, () => console.log(`Listenting to http://localhost:${PORT}...`))
